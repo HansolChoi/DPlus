42946722f44eecfbf8c5ef3bd977a3cf315f7be3
hansol choi <hanstemcell@naver.com>
2015-08-12 23:15:55 +0900

    줄바꿈

--- jni/Socket/socket.c
+++ jni/Socket/socket.c
@@ -20,7 +20,8 @@ typedef struct Socket
 Socket SocketList[SOCKETMAX];
 char server_addr[30];
 
-int CreateSocket(int ip, int port){
+int CreateSocket(int ip, int port)
+{
 	int CreateSockfd;
 	int SocketPortNumber = port - PORT_NUMBER;
 	struct hostent *pHostEnt;
@@ -42,6 +43,7 @@ int CreateSocket(int ip, int port){
 	pHostEnt = gethostbyname(server_addr);
 	SocketList[SocketPortNumber].svr_addr.sin_addr.s_addr = inet_addr(inet_ntoa(*((struct in_addr *)pHostEnt->h_addr)));
 
+	// connet() need to plan about blocking
 	if(connect(CreateSockfd, (struct sockaddr*)&SocketList[SocketPortNumber].svr_addr, sizeof(SocketList[SocketPortNumber].svr_addr)) < 0)
 	{
 		LOG(ANDROID_LOG_DEBUG, "DEBUGGING", "Can't connect to the Server.");
@@ -53,10 +55,12 @@ int CreateSocket(int ip, int port){
 	return 1;
 }
 
-int SocketSends(int *data, int len, int port){
+int SocketSends(int *data, int len, int port)
+{
 	int SocketPortNumber = port - PORT_NUMBER;
 
-	if (write(SocketList[SocketPortNumber].sockfd, str, len) < 0) {
+	if (write(SocketList[SocketPortNumber].sockfd, str, len) < 0)
+	{
 	   LOG(ANDROID_LOG_DEBUG, "DEBUGGING", "Writing Error.");
 	   fsync(SocketList[SocketPortNumber].sockfd);
 	   return 0;
@@ -64,18 +68,20 @@ int SocketSends(int *data, int len, int port){
 	 return 1;
 }
 
-int SocketReceives(int* data, int len, int port){
+int SocketReceives(int* data, int len, int port)
+{
 	int retval;
 	int SocketPortNumber = port - PORT_NUMBER;
 
 	retval = recv(SocketList[SocketPortNumber].sockfd, data, len, 0);
-	if (retval == -1) {
+	if (retval == -1)
 	   LOG(ANDROID_LOG_DEBUG, "DEBUGGING", "Receive Error.");
-	}
+
 	LOG(ANDROID_LOG_DEBUG, "DEBUGGING", "%d", data[0]);
 }
 
-void SocketStop(int port){
+void SocketStop(int port)
+{
 	int SocketNumber = port - PORT_NUMBER;
 
 	if(SocketList[SocketNumber].port == port)
